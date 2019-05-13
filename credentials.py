ade36927c92dcdca1d979c93c831550745ceb052
Aisha Sharif <sharif_aisha@yahoo.com>
2019-05-13 08:58:24 +0300

    Initiate Credentials class with required values

--- credentials.py
+++ credentials.py
@@ -8,4 +8,10 @@ class Credentials:
     '''
     credential_list = []
 
+    def __init__(self, account_url, account_username, account_password):
+        self.account_url = account_url
+        self.account_username = account_username
+        self.account_password = account_password
+
+    
 
