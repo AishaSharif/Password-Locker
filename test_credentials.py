ea78cda671e03dbbfbfe219564c4e63aa23c275d
Aisha Sharif <sharif_aisha@yahoo.com>
2019-05-13 09:16:38 +0300

    create a setUp function to define instructions to be run before each test case

--- test_credentials.py
+++ test_credentials.py
@@ -7,3 +7,10 @@ class TestCredentials(unittest.TestCase):
     class that defines test cases for the Credentials class
     '''
 
+    def setUp(self):
+        '''
+        function to define instructions to be run before each test case
+        '''
+        self.new_credentials = Credentials(
+            'http://twitter.com', 'nntaleb', 'ntaleb123')
+
